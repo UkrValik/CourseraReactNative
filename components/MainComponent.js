41570193a85f66de70a6652cfb6b96ff034fe570
Valentyn <krasniukevich@gmail.com>
2020-07-06 18:15:02 +0300

    Navigation Part 1

---
+++ components/MainComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Platform } from 'react-native';
+import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail },
+}, {
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff',
+        },
+    }
+});
+
+class Main extends Component {
+  render() {
+ 
+    return (
+        <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+            <MenuNavigator/>    
+        </View>
+    );
+  }
+}
+  
+export default Main;
